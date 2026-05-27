cfb93517c04dadef0b8009024a488cdba12ced28
Spiro <javastrydo56@gmail.com>
2026-05-27 15:12:36 +0530

    fixed deployment issue

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
+
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({
+      presets: [reactCompilerPreset()]
+    })
   ],
 })
